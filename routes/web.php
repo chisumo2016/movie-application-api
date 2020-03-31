676ff8f3ce9dab15dd1399f06640aae529c0887e
Bernard Chisumo <bchisumo74@gmail.com>
2020-03-31 23:03:44 +0100

    Pulling Movies  from API TMDB and dispaly of UI via HTTP Client

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'MoviesController@index')->name('movies.index');
-Route::get('/movies/{movie}', 'MoviesController @show')->name('movies.show');
+Route::get('/movies/{movie}', 'MoviesController@show')->name('movies.show');
 
