d0c6c15bceac359586b85e21637c1adc6e9950b0
Bernard Chisumo <bchisumo74@gmail.com>
2020-04-24 14:29:17 +0100

    Implemented the TV Show Module to appplication

--- app/ViewModels/ActorViewModel.php
+++ app/ViewModels/ActorViewModel.php
@@ -44,15 +44,28 @@ class ActorViewModel extends ViewModel
     {
          $castMovies = collect($this->credits)->get('cast');
 
-         return collect($castMovies)->where('media_type','movie')->sortByDesc('popularity')
-               ->take(5)->map(function ($movie){
-                     return collect($movie)->merge([
-                         'poster_path'  => $movie['poster_path']
-                             ?  'https://image.tmdb.org/t/p/w185/'. $movie['poster_path']
-                             :  'https://via.placeholder.com/185x278',
-                         'title'=> isset($movie['title']) ? $movie['title'] : 'Untitled',
-                     ]);
-             })->dump();
+         //return collect($castMovies)->where('media_type','movie')->sortByDesc('popularity')
+         return collect($castMovies)->sortByDesc('popularity')->take(5)->map(function ($movie){
+
+             if (isset($movie['title'])){
+                 $title  = $movie['title'];
+             }elseif (isset($movie['name'])){
+                 $title  = $movie['name'];
+             }else{
+                 $title = 'Untitled';
+             }
+
+             return collect($movie)->merge([
+                 'poster_path'  => $movie['poster_path']
+                     ?  'https://image.tmdb.org/t/p/w185/'. $movie['poster_path']
+                     :  'https://via.placeholder.com/185x278',
+                 'title'=> $title,
+                 'linkToPage'  => $movie['media_type'] === 'movie' ? route('movies.show', $movie['id']) : route('tv.show',$movie['id'])
+                 //'title'=> isset($movie['title']) ? $movie['title'] : 'Untitled',
+             ]);
+             })->only([
+                 'poster_path','title','id','media_type','linkToPage'
+         ])->dump();
     }
 
     public  function  credits()
@@ -85,6 +98,7 @@ class ActorViewModel extends ViewModel
                     'title'   => $title,
                     'character'   => isset($movie['character']) ? $movie['character'] : ''
                 ]);
-            })->sortByDesc('release_date')->dump();
+            })->sortByDesc('release_date');  // ->dump()
+
     }
 }
--- resources/views/actors/show.blade.php
+++ resources/views/actors/show.blade.php
@@ -25,7 +25,7 @@
 
                 @if ($social['twitter'])
                     <li class="ml-6">
-                        <a href="{{ $actor['twitter'] }}" title="Twitter">
+                        <a href="{{ $social['twitter'] }}" title="Twitter">
                             <svg class="fill-current text-gray-400 hover:text-white w-6" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg>
                         </a>
                     </li>
@@ -64,27 +64,10 @@
                     @foreach( $knownForMovies as $movie)
 
                         <div class="mt-4">
-                            <a href="{{ route('movies.show', $movie['id']) }}"><img src="{{ $movie['poster_path'] }}" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>
-                            <a href="{{ route('movies.show', $movie['id']) }}" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">{{ $movie['title'] }}</a>
+                            <a href="{{  $movie['linkToPage'] }}"><img src="{{ $movie['poster_path'] }}" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>
+                            <a href="{{ $movie['linkToPage'] }}" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">{{ $movie['title'] }}</a>
                         </div>
                     @endforeach
-
-{{--                    <div class="mt-4">--}}
-{{--                        <a href="#"><img src="https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>--}}
-{{--                        <a href="#" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">The Avengers</a>--}}
-{{--                    </div>--}}
-{{--                    <div class="mt-4">--}}
-{{--                        <a href="#"><img src="https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>--}}
-{{--                        <a href="#" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">The Avengers</a>--}}
-{{--                    </div>--}}
-{{--                    <div class="mt-4">--}}
-{{--                        <a href="#"><img src="https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>--}}
-{{--                        <a href="#" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">The Avengers</a>--}}
-{{--                    </div>--}}
-{{--                    <div class="mt-4">--}}
-{{--                        <a href="#"><img src="https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>--}}
-{{--                        <a href="#" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">The Avengers</a>--}}
-{{--                    </div>--}}
 
                 </div>
     </div>
@@ -107,3 +90,12 @@
 
 </div><!-- End of Image Movie-->
 @endsection
+
+{{--                    <div class="mt-4">--}}
+{{--                        <a href="#"><img src="https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>--}}
+{{--                        <a href="#" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">The Avengers</a>--}}
+{{--                    </div>--}}
+
+
+{{--<a href="{{ route('movies.show', $movie['id']) }}"><img src="{{ $movie['poster_path'] }}" alt="poster" class="hover:opacity-75 transition ease-in-out duration-150"></a>--}}
+{{--<a href="{{ route('movies.show', $movie['id']) }}" class="text-sm leading-normal block text-gray-400 hover:text-white mt-1">{{ $movie['title'] }}</a>--}}
